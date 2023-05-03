1331e4762726f6bfb494dab6871dd91aab8a5f62
Kyle burns <datasskyle@gmail.com>
2023-05-03 17:10:59 +0100

    Added edit product button when basket is empty

--- src/components/product/list-products.jsx
+++ src/components/product/list-products.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from 'react';
 
 import { getAll } from '../../services/products.service';
 
+import { useBasket } from '../basket/basket.context';
 
 import { Product } from './produst';
 
 export function ProductList() {
+    const { basket } = useBasket()
     const [products, setProducts] = useState([]);
+    const [editProducts, setEditProducts] = useState(false);
 
     useEffect(() => {
         const allProducts = getAll();
@@ -14,11 +17,26 @@ export function ProductList() {
         setProducts(allProducts);
     }, []);
 
+    const basketIsEmpty = basket.length;
+
+    function toggelEditProducts() {
+        setEditProducts(!editProducts);
+    }
+
     return (
         <>
-            {products && products.length > 0 ?
-                products.map((product) => <Product key={product.id} product={product} />)
-                : null}
+            {   /* edit product button */
+                !basketIsEmpty ? <button onClick={() => setEditProducts(!editProducts)}>
+                    {editProducts ? "Checkout" : "Edit Products"}
+                </button >
+                    : null
+            }
+
+            {   /* display all the products */
+                !editProducts && products && products.length > 0 ?
+                    products.map((product) => <Product key={product.id} product={product} />)
+                    : null
+            }
         </>
     );
 }
